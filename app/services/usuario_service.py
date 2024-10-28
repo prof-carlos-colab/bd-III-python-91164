63eb8f921986007692d9d12b0417ec1e80a26a84
prof-carlos-colab <profcarloscolab@gmail.com>
2024-10-28 12:18:57 +0000

    Add usuario_service.py

---
+++ app/services/usuario_service.py
@@ -0,0 +1,31 @@
+from models.usuario_model import Usuario
+from repositories.usuario_repository import UsuarioRepositoy
+
+class UsuarioService:
+
+    def __init__(self, respository: UsuarioRepositoy):
+        self.repository = respository
+
+    
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try:
+            usuario = Usuario(nome=nome, email=email, senha=senha)
+
+            novo_usuario = self.repository.pesquisar_usuario_por_email(usuario.email)
+
+            if not novo_usuario:
+                print("Usuário já cadastrado!")
+                return
+
+            self.repository.salvar_usuario(usuario)
+            print("Usuário cadastrado com sucesso!")
+        except TypeError as erro:
+            print(f"Erro ao salvar o usuário: {erro}")
+        except Exception as erro:
+            print(f"Ocorreu um erro inesperado: {erro}")
+
+    
+    def listar_todos_usuarios(self):
+        return self.repository.listar_usuarios()
+
+            
